41a6afed0ff62af67863593ecce49aec29a42f34
Rahul Bagul <rahul07bagul@gmail.com>
2024-12-01 16:09:48 -0500

    removed file

--- temp.py
+++ temp.py
@@ -1,10 +1,22 @@
-import bcrypt    
-def hash_password(password):
-    """
-    Hashes a password using bcrypt.
-    """
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password.decode('utf-8')
+import streamlit as st
+from streamlit_cookies_controller import CookieController
 
-print(hash_password('student'))
+st.set_page_config('Cookie QuickStart', '🍪', layout='wide')
+
+controller = CookieController()
+
+# Set a cookie
+controller.set('cookie_name', 'testing')
+st.write(st.session_state)
+
+# Get all cookies
+cookies = controller.getAll()
+st.write(cookies)
+
+# Get a cookie
+cookie = controller.get('cookie_name')
+st.write(cookie)
+
+# Remove a cookie
+controller.remove('cookie_name')
+st.write(st.session_state)
